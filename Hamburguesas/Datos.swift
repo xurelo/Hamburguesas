2bf2dcb33bb4648228576f0b84634b37efca8ac9
Francisco Manuel Romero <fran@frans-pro.local>
2016-04-21 23:31:55 +0200

    First draft

---
+++ Hamburguesas/Datos.swift
@@ -0,0 +1,9 @@
+//
+//  Datos.swift
+//  Hamburguesas
+//
+//  Created by Francisco Manuel Romero on 21/04/16.
+//  Copyright © 2016 Francisco Manuel Romero. All rights reserved.
+//
+
+import Foundation
